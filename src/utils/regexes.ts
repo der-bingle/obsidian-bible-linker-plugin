a1375d70fa39bcc4af90ded8e5a45cf0c2d1c4e5
der-bingle <luke.m.wilson@gmail.com>
2025-02-10 18:20:56 -0600

    Update regex patterns to support additional dash characters for verse and chapter ranges

--- src/utils/regexes.ts
+++ src/utils/regexes.ts
@@ -6,18 +6,18 @@
 export const oneVerseRegEx = new RegExp(/([^,:#]+)[,#.:;]\s*(\d+)\s*$/);
 
 // Link to multiple verses, for example "Gen 1,1-5"
-export const multipleVersesRegEx = new RegExp(/([^,:#]+)[,#.:;]\s*(\d+)\s*[-.=]\s*(\d+)\s*$/);
+export const multipleVersesRegEx = new RegExp(/([^,:#]+)[,#.:;]\s*(\d+)\s*[-.=–]\s*(\d+)\s*$/);
 
 // Book and chapter string
 export const bookAndChapterRegEx = /([^,:#]*\S)[-|\s]+(\d+)/
 
 // Multiple chapters, for example "Gen 1-3"
-export const multipleChaptersRegEx = /(\d*[^\d,:#]+)\s*(\d+)\s*-\s*(\d+)\s*$/
+export const multipleChaptersRegEx = /(\d*[^\d,:#]+)\s*(\d+)\s*[-–]\s*(\d+)\s*$/
 
 // Can be used to determine whether given name of file is from OBSK (for example Gen-01)
 export const isOBSKFileRegEx = /([A-zÀ-ž0-9 ]+)-(\d{2,3})/
 
 // Escapes given string so that it can be safely used in regular expression
 export function escapeForRegex(string: string) {
-	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
